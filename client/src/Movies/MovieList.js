59a8392c247b0f02613c2ece5d918a6c3bf0a9a6
VulpineReynard <milare8@gmail.com>
2019-07-22 19:41:02 -0400

    wip: getting id's for movie component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -20,7 +21,7 @@ const MovieList = props => {
   
   return (
     <div className="movie-list">
-      {this.state.movies.map(movie => (
+      {movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
@@ -29,9 +30,11 @@ const MovieList = props => {
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
+  const movieId = "/movies/" + movie.id;
+  console.log(movie.id);
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
+      <Link to={movieId}>{title}</Link>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
